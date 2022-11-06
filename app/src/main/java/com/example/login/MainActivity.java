48f4acda615d2e3bf96419c030afdec41a5e2541
unknown <csaponp1@miumg.edu.gt>
2022-11-05 22:36:53 -0600

    Primer_commit: creacion de login y machote de catalogo

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.login;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void lanzarmensaje(View view){
+        Intent intent = new Intent(MainActivity.this,SegundaActividad.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        };
+
+
+    }
+
